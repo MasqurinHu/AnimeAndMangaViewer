925e9a32e45abd7928367a8e21637615c1800198
MasqurinHu <Masqurin.Hu@access-company.com>
2020-12-23 16:20:46 +0800

    Initial Commit

---
+++ AnimeAndMangaViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeAndMangaViewer
+//
+//  Created by 五加一 on 2020/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
